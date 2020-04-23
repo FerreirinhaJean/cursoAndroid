02fcce27ef92da7088037d8b80141629338b1ba1
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-04-23 14:11:54 -0300

    Repositorio projetos android criado

---
+++ AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirDialog(View view) {
+        //Intanciar AlertDialog
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        //Configurar titulo e mensagem
+        dialog.setTitle("Titulo dialog");
+        dialog.setMessage("Mensagem dialog");
+
+        //Configurar cancelamento
+        dialog.setCancelable(false);
+
+        //Configurar icone
+        dialog.setIcon(android.R.drawable.ic_btn_speak_now);
+
+        //Configura ações para sim e não
+        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Executar ação botão sim", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Executar ação botão não", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.create();
+        dialog.show();
+    }
+}
