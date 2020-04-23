de60f1b422feaccac3824743d9e143550841a927
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-04-23 14:36:14 -0300

    Projeto SeekBar concluido

--- SeekBar/app/src/main/java/com/example/seekbar/MainActivity.java
+++ SeekBar/app/src/main/java/com/example/seekbar/MainActivity.java
@@ -1,14 +1,48 @@
-irinhpackage com.example.seekbar;
+package com.example.seekbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBarEscala;
+    private TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBarEscala = findViewById(R.id.seekBarEscala);
+        tvResultado = findViewById(R.id.tvResultado);
+
+
+        seekBarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //tvResultado.setText("onProgressChanged");
+                tvResultado.setText("Progresso: " + progress + " / " + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //tvResultado.setText("onStartTrackingTouch");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // tvResultado.setText("onStopTrackingTouch");
+            }
+        });
     }
+
+
+    public void recuperarProgresso(View view) {
+        tvResultado.setText("Escolhido: " + seekBarEscala.getProgress());
+    }
+
 }
