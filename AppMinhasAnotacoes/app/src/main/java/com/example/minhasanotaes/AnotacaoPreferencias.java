364a50f7eb337392e4e265095beae5e74ead6889
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-04-29 14:27:27 -0300

    App Minhas Anotacoes finalizado

---
+++ AppMinhasAnotacoes/app/src/main/java/com/example/minhasanotaes/AnotacaoPreferencias.java
@@ -0,0 +1,30 @@
+package com.example.minhasanotaes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AnotacaoPreferencias {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private final String NOME_ARQUIVO = "anotacao.preferencias";
+    private final String CHAVE_NOME = "nome";
+
+    public AnotacaoPreferencias(Context context) {
+        this.context = context;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+    }
+
+    public void salvarAnotacao(String anotacao) {
+        editor.putString(CHAVE_NOME, anotacao);
+        editor.commit();
+    }
+
+    public String recuperarAnotacao() {
+        return preferences.getString(CHAVE_NOME,"");
+    }
+
+}
