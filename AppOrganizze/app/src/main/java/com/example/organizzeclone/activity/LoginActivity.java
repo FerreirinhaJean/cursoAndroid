6acc523b6fa4d43a4128dde928f6c8000f8c0e71
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-05-14 09:01:39 -0300

    FloatingActionMenu projeto

--- AppOrganizze/app/src/main/java/com/example/organizzeclone/activity/LoginActivity.java
+++ AppOrganizze/app/src/main/java/com/example/organizzeclone/activity/LoginActivity.java
@@ -1,16 +1,96 @@
 package com.example.organizzeclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.organizzeclone.R;
+import com.example.organizzeclone.config.ConfiguracaoFireBase;
+import com.example.organizzeclone.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etEmailLogin, etSenhaLogin;
+    private Button btnEntrar;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmailLogin = findViewById(R.id.etEmailLogin);
+        etSenhaLogin = findViewById(R.id.etSenhaLogin);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmailLogin.getText().toString();
+                String senha = etSenhaLogin.getText().toString();
+
+                if (!email.isEmpty()) {
+                    if (!senha.isEmpty()) {
+                        Usuario usuario = new Usuario(email, senha);
+                        validaLogin(usuario);
+
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Preencha a senha!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "Preecha o email!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
+
+    public void validaLogin(Usuario usuario) {
+        autenticacao = ConfiguracaoFireBase.getFireBaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    abrirTelaPrincipal();
+                } else {
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        excecao = "Usuário não está cadastrado";
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        excecao = "Email/Senha não correspondem a um usuario cadastrado";
+                    } catch (Exception e) {
+                        excecao = "Erro ao fazer login: " + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void abrirTelaPrincipal() {
+        startActivity(new Intent(LoginActivity.this, PrincipalActivity.class));
+        finish();
+    }
+
+
 }
--- AppOrganizze/app/src/main/java/com/example/organizzeclone/activity/MainActivity.java
+++ AppOrganizze/app/src/main/java/com/example/organizzeclone/activity/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.organizzeclone.R;
+import com.example.organizzeclone.config.ConfiguracaoFireBase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
@@ -13,10 +15,13 @@ public class MainActivity extends IntroActivity {
 
     private Button btnCadastrar;
 
+    private FirebaseAuth autenticacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_main);
+
 
         setButtonBackVisible(false);
         setButtonNextVisible(false);
@@ -54,12 +59,30 @@ public class MainActivity extends IntroActivity {
     }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        verificarUsuarioLogado();
+    }
+
     public void btnEntrar(View v) {
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 
     public void btnCadastrar(View v) {
         startActivity(new Intent(MainActivity.this, CadastroActivity.class));
+    }
+
+    public void verificarUsuarioLogado() {
+        autenticacao = ConfiguracaoFireBase.getFireBaseAutenticacao();
+        if (autenticacao.getCurrentUser() != null) {
+            abrirTelaPrincipal();
+        }
+    }
+
+    public void abrirTelaPrincipal() {
+        startActivity(new Intent(MainActivity.this, PrincipalActivity.class));
+        finish();
     }
 
 }
