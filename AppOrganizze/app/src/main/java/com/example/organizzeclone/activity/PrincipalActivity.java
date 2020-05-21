3978f4ba89f7493a85b414de20ecaadadf24b14d
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-05-21 10:04:11 -0300

    App organizze - configuracao toolbar

--- AppOrganizze/app/src/main/java/com/example/organizzeclone/activity/PrincipalActivity.java
+++ AppOrganizze/app/src/main/java/com/example/organizzeclone/activity/PrincipalActivity.java
@@ -3,24 +3,47 @@ package com.example.organizzeclone.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.organizzeclone.config.ConfiguracaoFireBase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.organizzeclone.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private TextView tvSaldo, tvSaudacao;
+    private RecyclerView recyclerMovimentos;
+    private MaterialCalendarView calendarView;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
+
+
+        calendarView = findViewById(R.id.calendarView);
+        tvSaldo = findViewById(R.id.tvSaldo);
+        tvSaudacao = findViewById(R.id.tvSaudacao);
+
+        configuraCalendarView();
 
        /* FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,12 +55,45 @@ public class PrincipalActivity extends AppCompatActivity {
         });*/
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuSair:
+                autenticacao = ConfiguracaoFireBase.getFireBaseAutenticacao();
+                autenticacao.signOut();
+                startActivity(new Intent(this,MainActivity.class));
+                finish();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void adicionarDespesa(View view) {
         startActivity(new Intent(PrincipalActivity.this, DespesasActivity.class));
     }
 
     public void adicionarReceita(View view) {
         startActivity(new Intent(PrincipalActivity.this, ReceitasActivity.class));
+    }
+
+    public void configuraCalendarView() {
+        CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 
 }
